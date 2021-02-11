c51f300a6f267958859bcc40d63ef20e05804a11
Yasu1025 <sillyjp25@gmail.com>
2021-02-11 11:57:51 +0900

    【Feature】add comments

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,6 +7,8 @@ import { selectUser } from '../features/userSlice'
 import { Avatar } from '@material-ui/core'
 import MessageIcon from '@material-ui/icons/Message'
 import SendIcon from '@material-ui/icons/Send'
+import { Unsubscribe } from "@material-ui/icons";
+import { makeStyles } from '@material-ui/core/styles';
 
 interface PROPS {
   postId: string;
@@ -15,9 +17,75 @@ interface PROPS {
   text: string;
   timestamp: any;
   username: string;
-}
+};
+
+interface COMMENT {
+  id: string;
+  avatar: string;
+  text: string;
+  timestamp: any;
+  username: string
+};
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
 
 const Post: React.FC<PROPS> = (props) => {
+  const classes = useStyles();
+  const user = useSelector(selectUser);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState<COMMENT[]>([
+    {
+      id: "",
+      avatar: "",
+      text: "",
+      timestamp: null,
+      username: ""
+    }
+  ]);
+  const [openComments, setOpenComments] = useState(false);
+
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection('posts').doc(props.postId).collection('comments').add({
+      avatar: user.photoURL,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName
+    });
+    setComment('');
+  };
+
+  useEffect(() => {
+    const unSub = db
+      .collection('posts')
+      .doc(props.postId)
+      .collection('comments')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => (
+            {
+              id: doc.id,
+              avatar: doc.data().avatar,
+              text: doc.data().text,
+              timestamp: doc.data().timestamp,
+              username: doc.data().username
+            }
+          ))
+        );
+      });
+      return () => {
+        unSub();
+      }
+
+  }, [props.postId])
+
   return (
     <div className={styles.post}>
       <div className={styles.post_avatar}>
@@ -37,11 +105,50 @@ const Post: React.FC<PROPS> = (props) => {
             <p>{ props.text }</p>
           </div>
         </div>
+
         { props.image && (
           <div className={styles.post_tweetImage}>
             <img src={props.image} alt="tweet" />
           </div>
         )}
+
+        <MessageIcon
+          className={styles.post_commentIcon}
+          onClick={() => setOpenComments(!openComments)}
+        />
+      {openComments && (
+        <>
+        { comments.map((com) => (
+          <div key={com.id} className={styles.post_comment}>
+            <Avatar src={com.avatar} className={classes.small} />
+            <span className={styles.post_commentUser}>@{ com.username }</span>
+            <span className={styles.post_commentText}>{ com.text }</span>
+            <span className={styles.post_headerTime}>
+              { new Date(com.timestamp?.toDate()).toLocaleString() }
+            </span>
+          </div>
+        ))}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              type="text"
+              className={styles.post_input}
+              placeholder="Type comment..."
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setComment(e.target.value)
+              }}
+            />
+            <button
+              className={ comment ? styles.post_button : styles.post_buttonDisable}
+              type="submit"
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
+        </>
+      )}
       </div>
     </div>
   )
